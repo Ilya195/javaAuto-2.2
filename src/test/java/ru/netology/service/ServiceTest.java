d1aa9d4e5ed03817d25c8be58b6e65c6b087a23a
Ilya195 <vodoley_agent@inbox.ru>
2022-08-07 15:39:54 +0600

    Initial commit

---
+++ src/test/java/ru/netology/service/ServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import static org.hamcrest.Matchers.*;
+//import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+public class ServiceTest {
+
+@Test
+        void shouldBody() {
+    //Given - When - Then
+// Предусловия
+    given()
+            .baseUri("https://postman-echo.com")
+            .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+            .when()
+            .post("/post")
+// Проверки
+            .then()
+            .statusCode(200)
+            .body("JsonPath", equalTo(468));
+}         ;
+}
